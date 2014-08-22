9599df8322e026d394be5c48192e9ba232369ae4
Wush Wu <wush978@gmail.com>
2014-08-22 21:25:54 +0800

    init WushUtility

---
+++ src/ompSum.cpp
@@ -0,0 +1,12 @@
+#include <Rcpp.h>
+using namespace Rcpp;
+
+// [[Rcpp::export("ompSum.numeric")]]
+double ompSum(NumericVector src) {
+  double retval = 0;
+  #pragma omp parallel for reduction (+ : retval)
+  for(int i = 0;i < src.size();i++) {
+    retval += src[i];
+  }
+  return retval;
+}
